a5aad0343d908ff49ab1c89c1208c0b7977a980a
Filippoalotta <alottafilippo@gmail.com>
2024-04-11 18:35:34 +0200

    prova 4

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'classic' // Specifica il runtime JSX da utilizzare (opzionale)
-    })
-  ],
-  server: {
-    fs: {
-      strict: false // Configurazione per gestire i file ".jsx" come moduli JavaScript
-    }
-  }
-});
+  plugins: [react()],
+})
