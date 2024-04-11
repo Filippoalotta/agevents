5e083d84bc503db5e3ed08e379a0f39940e68097
Filippoalotta <alottafilippo@gmail.com>
2024-04-11 18:29:49 +0200

    prova vite 2

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react({
+      jsxRuntime: 'classic' // Specifica il runtime JSX da utilizzare (opzionale)
+    })
   ],
   server: {
     fs: {
-      strict: false
+      strict: false // Configurazione per gestire i file ".jsx" come moduli JavaScript
     }
   }
 });
